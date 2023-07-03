4664557c3b111057b9dbc77fc718534ae5c85f9d
Светлана Суворова <myrrh@myrrh.ru>
2023-07-03 18:05:08 +0300

    Fix: исправить ошибки после автотеста

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,12 +3,6 @@ const UnauthorizedError = require('../errors/UnauthorizedError');
 const { JWT_SECRET } = require('../utils/utils');
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError('Необходима авторизация');
-  }
-
   const token = req.cookies.jwt;
   let payload;
 
